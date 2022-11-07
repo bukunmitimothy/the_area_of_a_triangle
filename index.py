3e8c88eb762d268b1b93595798a39b7b71f7b1b9
bukunmitimothy <102929055+bukunmitimothy@users.noreply.github.com>
2022-11-07 13:55:11 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,7 @@
+# calculating the area of a triangle 6 times
+
+for i in range(6):
+  height = input("what is the height: ")
+  base = input("what is the base: ")
+  triangle =  (int(height) * int(base)) / 2
+  print("the area of the triangle is", triangle)
